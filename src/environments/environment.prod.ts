d1f995048434f2f770312f34f6bdf879aeab13bc
Alberto Basalo <albertobasalo@hotmail.com>
2018-12-12 15:50:41 +0100

    pwa

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api-base.herokuapp.com/api/',
-  assetsUrl: 'http://localhost:4007/assets/',
+  assetsUrl: 'http://127.0.0.1:8080/assets/',
   buy: 'buy',
   sell: 'sell'
 };

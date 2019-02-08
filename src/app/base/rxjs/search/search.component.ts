e1b0ae18167be3a3265cef5f08bf9da0d5ace14a
Alberto Basalo <albertobasalo@hotmail.com>
2019-02-08 10:57:18 +0100

    from event async

--- src/app/base/rxjs/search/search.component.ts
+++ src/app/base/rxjs/search/search.component.ts
@@ -1,11 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { fromEvent, Observable } from 'rxjs';
+import { fromEvent, Observable, of } from 'rxjs';
 import {
+  catchError,
   debounceTime,
   distinctUntilChanged,
   map,
-  switchMap
+  switchMap,
+  take
 } from 'rxjs/operators';
 
 @Component({
@@ -14,32 +16,52 @@ import {
   styles: []
 })
 export class SearchComponent implements OnInit {
-  @ViewChild('source') source: ElementRef;
-  keys$: Observable<any>;
-  debounce$: Observable<any>;
-  distinct$: Observable<any>;
-  launches$: Observable<any[]>;
-  numberOfLaunches$: Observable<number>;
+  @ViewChild('source') public source: ElementRef;
+  public keys$: Observable<any>;
+  public debounce$: Observable<any>;
+  public distinct$: Observable<any>;
+  public launches$: Observable<any[]>;
+  public clickLaunches$: Observable<any[]>;
 
   private url = 'https://launchlibrary.net/1.4/launch/';
-  private params = '?limit=200&mode=list';
+  private params = '?limit=2000&mode=list';
+
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {
+  public ngOnInit() {
     const el = this.source.nativeElement;
+    this.getNewSearchTerms(el);
+    this.searchTerm();
+    // this.getTermAndSearch(el);
+  }
+
+  public search(searchTerm: string) {
+    this.clickLaunches$ = this.callGet(searchTerm);
+  }
+
+  private getNewSearchTerms(el: any) {
     this.keys$ = fromEvent(el, 'keyup').pipe(map(() => el.value));
     this.debounce$ = this.keys$.pipe(debounceTime(400));
     this.distinct$ = this.debounce$.pipe(distinctUntilChanged());
-    this.launches$ = this.distinct$.pipe(
-      switchMap(term =>
-        this.http
-          .get<any>(`${this.url}${term}${this.params}`)
-          .pipe(map(result => result.launches))
-      )
-    );
-    this.numberOfLaunches$ = this.launches$.pipe(map(v => v.length));
   }
-  // To Do: get after 404
-  // share
-  // take(1)
+
+  private searchTerm() {
+    this.launches$ = this.distinct$.pipe(switchMap(this.callGet));
+  }
+
+  private getTermAndSearch(el: any) {
+    this.launches$ = fromEvent(el, 'keyup').pipe(
+      map(() => el.value),
+      debounceTime(400),
+      distinctUntilChanged(),
+      switchMap(this.callGet)
+    );
+  }
+
+  private callGet = (searchTerm: string) =>
+    this.http.get<any>(`${this.url}${searchTerm}${this.params}`).pipe(
+      take(1),
+      map(result => result.launches),
+      catchError(() => of([]))
+    );
 }

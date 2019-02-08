23edf09fd792117e302c56caeae5164920268744
Alberto Basalo <albertobasalo@hotmail.com>
2019-02-08 12:47:38 +0100

    maps

--- src/app/base/rxjs/maps/maps.component.ts
+++ src/app/base/rxjs/maps/maps.component.ts
@@ -17,7 +17,6 @@ import {
   styles: []
 })
 export class MapsComponent implements OnInit {
-  // https://medium.com/@shairez/a-super-ninja-trick-to-learn-rxjss-switchmap-mergemap-concatmap-and-exhaustmap-forever-88e178a75f1b
   private frameworks = ['Backbone', 'React', 'Angular'];
   public frameworks$: Observable<any>;
   public unflat$: Observable<any>;
@@ -29,59 +28,56 @@ export class MapsComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    const twitter$ = this.getFrameworks$(this.frameworks);
+    const twitter$ = this.getFrameworksTweets$(this.frameworks);
     this.frameworks$ = twitter$.pipe(
       tap(framework => console.log(`*** ${framework} tweet! ***`)),
       scan(this.accumulate)
     );
 
     this.unflat$ = twitter$.pipe(
-      map(framework => this.getRecruitsObservable(framework)),
+      map(framework => this.getFrameworkDevs$(framework)),
       tap(developer => console.log('unflat:' + developer))
     );
 
     this.mergeMap$ = twitter$.pipe(
-      mergeMap(framework => this.getRecruitsObservable(framework)),
+      mergeMap(framework => this.getFrameworkDevs$(framework)),
       tap(developer => console.log('mergeMap:' + developer)),
       scan(this.accumulate)
     );
 
     this.switchMap$ = twitter$.pipe(
-      switchMap(framework => this.getRecruitsObservable(framework)),
+      switchMap(framework => this.getFrameworkDevs$(framework)),
       tap(developer => console.log('switchMap:' + developer)),
       scan(this.accumulate)
     );
 
     this.concatMap$ = twitter$.pipe(
-      concatMap(framework => this.getRecruitsObservable(framework)),
+      concatMap(framework => this.getFrameworkDevs$(framework)),
       tap(developer => console.log('concatMap:' + developer)),
       scan(this.accumulate)
     );
 
     this.exhaustMap$ = twitter$.pipe(
-      exhaustMap(framework => this.getRecruitsObservable(framework)),
+      exhaustMap(framework => this.getFrameworkDevs$(framework)),
       tap(developer => console.log('exhaustMap:' + developer)),
       scan(this.accumulate)
     );
   }
 
-  private getFrameworks$(frameworkNames: string[]): Observable<string> {
-    return interval(4000).pipe(
+  private getFrameworksTweets$ = (frameworkNames: string[]) =>
+    interval(4000).pipe(
       map(index => frameworkNames[index]),
       take(frameworkNames.length)
     );
-  }
 
-  private getRecruitsObservable(framework: string) {
-    return interval(1500).pipe(
+  private getFrameworkDevs$ = (framework: string) =>
+    interval(1500).pipe(
       map(val => this.getMessage(framework, val)),
       take(4)
     );
-  }
 
-  private getMessage = (framework: string, val: number) =>
-    `${framework} dev ${val + 1}`;
+  private getMessage = (framework, val) => `${framework}Dev${val + 1}`;
 
-  private accumulate = (accumalated, current) =>
-    accumalated.concat(current + ' ');
+  private accumulate = (total, current) => total.concat(' ' + current + ' ');
 }
+// https://medium.com/@shairez/a-super-ninja-trick-to-learn-rxjss-switchmap-mergemap-concatmap-and-exhaustmap-forever-88e178a75f1b
